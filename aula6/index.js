9ed971680a33b0425625919aa6e44aea2111ce53
Ricardo Avakian <rcavakian@gmail.com>
2024-08-20 14:54:42 -0300

    Adicionar aula6 com regras de declaração de variáveis

---
+++ aula6/index.js
@@ -0,0 +1,8 @@
+// Não podemos criar variáveis com palavras reservadas
+// Variáveis precisam ter nomes significativos
+// Não pode começar o nome de uma variável com um número
+// Não podem conter espaços ou traços
+// Utilizar camelCase
+// A sintaxe é case-sensitive
+// Não podemos redeclarar variáveis com let
+// Não utilizar 'var', UTILIZAR 'let'

1c5897ce6ab03dc6ae70ad6e5628e1bc8a178f91
Ricardo Avakian <rcavakian@gmail.com>
2024-08-20 11:14:19 -0300

    Primeiro commit com aula3

---
+++ aula3/index.js
@@ -0,0 +1,3 @@
+console.log("'Ricardo Avakian'"); // String texto, envolver a tring em aspas duplas para poder utilizar aspas simples dentro da string
+console.log('Ricardo Avakian');
+console.log(`'Ricardo' "Avakian"`); // Envolver a string entre acentos graves " ` " para poder usar ao mesmo tempo aspas simples e duplas 

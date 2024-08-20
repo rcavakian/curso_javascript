9759776fef293d2e7b3843f82c60e775c465fc5b
Ricardo Avakian <rcavakian@gmail.com>
2024-08-20 13:50:30 -0300

    Adicinar diretorio aula5 com exemplo de utilização do arquivo .html e .js para armazenar o script

---
+++ aula5/js/index.js
@@ -0,0 +1,7 @@
+// Mnesagens para serem exibidas no console
+console.log('Olá');
+console.log('Este trecho será exibido no console');
+
+// Alertas
+alert('Olá mundo!');
+alert('Meu nome é Ricardo');

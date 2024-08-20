9c3bacb4e7e92bb4e64131e596f3ebf098a126d2
Ricardo Avakian <rcavakian@gmail.com>
2024-08-20 15:15:33 -0300

    Adicionar aula7 com regras de declaração de constantes

---
+++ aula7/index.js
@@ -0,0 +1,9 @@
+// Não podemos criar constantes com palavras reservadas
+// Constantes precisam ter nomes significativos
+// Não pode começar o nome de uma constanrte com um número
+// Não podem conter espaços ou traços
+// Utilizar camelCase
+// A sintaxe é case-sensitive
+// A constante ao ser criada precisa ser inicializada (atribuída um valor)
+// Não podemos modificar o valor de uma constante
+// Não utilizar 'var', UTILIZAR 'const'

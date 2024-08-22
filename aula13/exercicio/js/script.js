529e1f17f2c6c99e2d628a30df6c482a6af51ade
Ricardo Avakian <rcavakian@gmail.com>
2024-08-22 09:26:41 -0300

    Adicionar alguns ajustes no exercicio da aula13

--- aula13/exercicio/js/script.js
+++ aula13/exercicio/js/script.js
@@ -1,44 +1,44 @@
 const nome = prompt('Digite seu nome completo:');
 // Seu nome é:
-document.body.innerHTML = `Seu nome é: ${nome} <br />`;
+document.body.innerHTML = `Seu nome é: <strong>${nome}</strong><br />`;
 
 // Seu nome tem ______ letras
 const nomeSemEspaco = nome.replaceAll(' ', '');
 const quantidadeLetras = nomeSemEspaco.length;
-document.body.innerHTML += `Seu nome tem ${quantidadeLetras} letras.<br />`;
+document.body.innerHTML += `Seu nome tem <strong>${quantidadeLetras}</strong> letras.<br />`;
 
 
 // A segunda letra do seu nome é: ______
 const segundaLetra = nomeSemEspaco.charAt(1);
-document.body.innerHTML += `A segunda letra do seu nome é: ${segundaLetra}.<br />`;
+document.body.innerHTML += `A segunda letra do seu nome é: <strong>${segundaLetra}</strong>.<br />`;
 
 // Qual o primeiro índice da letra 'a' no seu nome? ______
 const indiceLetraA = nome.search('a');
-document.body.innerHTML += `Qual o primeiro índice da letra 'a' no seu nome? ${indiceLetraA}<br />`;
+document.body.innerHTML += `Qual o primeiro índice da letra 'a' no seu nome? <strong>${indiceLetraA}</strong><br />`;
 
 // Qual o último índice da letra LETRA no seu nome? ______
 const ultimoIndiceLetra = nome.lastIndexOf('a');
-document.body.innerHTML += `Qual o último índice da letra 'a' no seu nome? ${ultimoIndiceLetra}<br />`;
+document.body.innerHTML += `Qual o último índice da letra 'a' no seu nome? <strong>${ultimoIndiceLetra}</strong><br />`;
 
 // As últimas 3 letras do seu nome são: ______
 const ultimasTresLetras = nome.slice(-3);
-document.body.innerHTML += `As últimas 3 letras do seu nome são: ${ultimasTresLetras}<br />`;
+document.body.innerHTML += `As últimas 3 letras do seu nome são: <strong>${ultimasTresLetras}</strong><br />`;
 
 // As palavras do seu nome são: ______
 const palavrasNome = nome.split(' ');
 document.body.innerHTML += `As palavras do seu nome são: <br />`;
 
 for (let i = 0; i < palavrasNome.length; i++) {
-    document.body.innerHTML += `${(i+1)}a - ${palavrasNome[i]} <br />`;
+    document.body.innerHTML += `${(i+1)}a - <strong>${palavrasNome[i]}</strong> <br />`;
 }
 
 // Seu nome com letras maiúsculas: ______
 const nomeMaiuscula = nome.toUpperCase();
-document.body.innerHTML += `Seu nome com letras maiúsculas: ${nomeMaiuscula} <br />`;
+document.body.innerHTML += `Seu nome com letras maiúsculas: <strong>${nomeMaiuscula}</strong> <br />`;
 
 // Seu nome com letras minúsculas: ______
 const nomeMinuscula = nome.toLowerCase();
-document.body.innerHTML += `Seu nome com letras minúsculas: ${nomeMinuscula} <br />`;
+document.body.innerHTML += `Seu nome com letras minúsculas: <strong>${nomeMinuscula}</strong> <br />`;
 
 /**
  * Resultado esperado

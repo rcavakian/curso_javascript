fdc50f3ecbbb528e9dfd7286699599b98678aabb
Ricardo Avakian <rcavakian@gmail.com>
2024-08-23 15:21:16 -0300

    Adicionar aula24 Estruturas condicionais: if, else if e else

--- aula23/index.js
+++ aula23/index.js
@@ -2,6 +2,17 @@
 Avaliação de Curto-Circuito (Short-Circuit)
 
 FALSY - valores que são avaliados como 'false' quando necessário
+false
 0 - o valor zero
-'', "", `` 
+'', "", `` - strings vazias
+null / undefined
+NaN 
+
 */
+
+/*
+Na avaliação de short-cicuit, encontrar um valor 'false'/'FALSY' e o operador '&&'
+a avaliação é interrompida e retornada como 'false'.
+O mesmo vale para o 'true' com o operador '||', a operação é retornada como 'true' e encerrada.
+*/
+console.log('Luiz' && 'Maria')
---
+++ aula24/index.js
@@ -0,0 +1,8 @@
+/*
+Estruturas condicionais : if, else if, else
+
+if -> pode ser utilizao sozinho
+else -> sempre que utiliza a palavra 'else', preciso de um 'if' antes
+else if -> podemos ter vários na checagem
+else -> só podemos ter um else na checagem e sempre será no final
+*/
